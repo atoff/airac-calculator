4221a6ee3ae577f2fdd99aa8e449320d75134492
Alex Toff <me@alextoff.uk>
2023-07-29 11:29:57 +0000

    remove examples

--- examples/nextairac.php
+++
@@ -1,6 +0,0 @@
-<?php
-require('../vendor/autoload.php');
-
-use CobaltGrid\Aviation\AIRACCalculator;
-
-echo "The next effective AIRAC is " . AIRACCalculator::nextAiracCycle() . ", which becomes effective at " . AIRACCalculator::nextAiracDate()->format('d/m/Y');
